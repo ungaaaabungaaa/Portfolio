8c4fee9557e0ef0724d406bba150094fda1f6328
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-07-01 15:54:22 +0530

    added backdrop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
-   <h1 className='yeseva-one-regular'>Coming Soon</h1>
+    <div id='hero' className='hero'>
+      <h1 className='yeseva-one-regular'>Coming Soon</h1>
+    </div>
   );
 }
 
