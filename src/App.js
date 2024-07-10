a7b509f092ccc64c5ac6e1dbd2b71c3aab1bf6c0
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-07-10 17:51:22 +0530

    added swipe down logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div id='hero' className='hero'>
-      <h1 className='libre-franklin-h1'>Coming Soon</h1>
+      <div className='title_container'> 
+        <h1 className='libre-franklin-h1'>Syed Abdul Muqeeth</h1>
+        <h2 className='libre-franklin-h2 h2_margin_left_20 text_align_end'>Mechatronics Engineer</h2>
+      </div>
+      
     </div>
   );
 }
