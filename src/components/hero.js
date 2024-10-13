a9fbbce7e495fa35e8e10522045dd119bff25053
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-10-14 05:00:25 +0530

    added something duh

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import '../styles/hero.css';
-import Marquee from "react-fast-marquee";
+import '../styles/icons.css';
 
 function Hero() {
   return (
     <>
-    <div className='hero'></div>
+    <div className='hero'>
+      <div className='hero_titles'>
+        <h1><sup><span class="ph--star-four"></span></sup> SYED ABDUL MUQEETH</h1>
+        <h2>Mechatronics Enginner</h2>
+        <h3>Full Stack Developer | Machine Learning Engineer | Data Scientist | Cybersecurity Specialist | DevOps Engineer | UI/UX Designer | Project Manager | IT Support Specialist | Digital Marketing Expert|</h3>
+      </div>
+      <div className='hero_accordion_section'>
+        <div className='hero_accordion_section_1'>
+          
+        </div>
+        <div className='hero_accordion_section_2'></div>
+        <div className='hero_accordion_section_3'></div>
+        <div className='hero_accordion_section_4'></div>
+        <div className='hero_accordion_section_5'></div>
+      </div>
+
+      
+    </div>
     </>
   );
 }
